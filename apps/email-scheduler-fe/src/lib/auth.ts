3d0a4fd3b322d25f5770ad2f6b88783a7f6eb48b
Tarun Karmakar <tarun.karmakar.dev@gmail.com>
2025-01-30 20:00:36 +0530

    Email scheduler Add lib/auth

--- apps/email-scheduler-fe/src/lib/auth.ts
+++ apps/email-scheduler-fe/src/lib/auth.ts
@@ -2,6 +2,7 @@ import "server-only";
 import * as jose from "jose";
 import { getEnv } from "./env";
 import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
 const env = getEnv();
 
@@ -32,13 +33,33 @@ export async function createSession(user: { id: string }) {
   });
 }
 export async function verifyIsAuthenticated(request: Request) {
-  const cookieStore = cookies();
-  const token = cookieStore.get("token");
-  if (!token) return null;
   try {
+    const cookieStore = cookies();
+    const token = cookieStore.get("token");
+    if (!token) return null;
     const { payload } = await verifyAuthToken(token.value);
     return payload.id as string;
   } catch (e) {
     return null;
   }
+}
+
+export async function signOut() {
+  const cookieStore = cookies();
+  cookieStore.delete("token");
+}
+
+export function createProtectedRouteHandler<T>(
+  handler: (
+    request: NextRequest,
+    userId: string
+  ) => Promise<NextResponse<T>> | NextResponse<T>
+) {
+  return async (request: NextRequest) => {
+    const userId = await verifyIsAuthenticated(request);
+    if (!userId) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    return await handler(request, userId);
+  };
 }
