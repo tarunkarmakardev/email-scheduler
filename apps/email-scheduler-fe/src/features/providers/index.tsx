963367674f0b1f530ca628ec464904b086f21d35
Tarun Karmakar <tarun.karmakar.dev@gmail.com>
2025-02-02 16:18:14 +0530

    Email Scheduler FE Update providers

--- apps/email-scheduler-fe/src/features/providers/index.tsx
+++ apps/email-scheduler-fe/src/features/providers/index.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { ThemeProvider } from "@email-scheduler/ui";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ThemeProvider>
   );
 }
