b023ccade7f7349994b56a3ffd7712db8fbf83b3
Tarun Karmakar <tarun.karmakar.dev@gmail.com>
2025-02-02 15:31:00 +0530

    Add libs/hooks Add mobile hook

---
+++ libs/hooks/jest.config.ts
@@ -0,0 +1,17 @@
+export default {
+  displayName: "hooks",
+  preset: "../../jest.preset.js",
+  transform: {
+    "^.+\\.[tj]sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: { syntax: "typescript", tsx: true },
+          transform: { react: { runtime: "automatic" } },
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/libs/hooks",
+};
