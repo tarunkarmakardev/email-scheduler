dbf8926f8a55e2f5728c81579184ae7855938dc5
Tarun Karmakar <tarun.karmakar.dev@gmail.com>
2025-02-02 13:14:09 +0530

    Update project.json to add ts-lint task Add global script ts-lint-all, lint-all

--- apps/email-scheduler-fe-e2e/src/e2e/app.cy.ts
+++ apps/email-scheduler-fe-e2e/src/e2e/app.cy.ts
@@ -1,11 +1,10 @@
-import { getGreeting } from '../support/app.po';
+import { getGreeting } from "../support/app.po";
 
-describe('email-scheduler-fe-e2e', () => {
-  beforeEach(() => cy.visit('/'));
+describe("email-scheduler-fe-e2e", () => {
+  beforeEach(() => cy.visit("/"));
 
-  it('should display welcome message', () => {
+  it("should display welcome message", () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains(/Welcome/);
--- apps/email-scheduler-fe/src/app/auth/sign-in/page.tsx
+++ apps/email-scheduler-fe/src/app/auth/sign-in/page.tsx
@@ -2,12 +2,11 @@ import { query } from "@/lib/query";
 import { GoogleAuthResponse } from "@/schemas/google";
 
 export default async function Page() {
-  const {
-    res: { authUrl },
-  } = await query<GoogleAuthResponse>("/api/auth/google");
+  const { res } = await query<GoogleAuthResponse>("/api/auth/google");
+  if (!res) return <div>Error</div>;
   return (
     <div>
-      <a href={authUrl} className="underline text-blue-400">
+      <a href={res.authUrl} className="underline text-blue-400">
         Sign in With Google
       </a>
     </div>
