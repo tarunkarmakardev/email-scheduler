be158b546702d2405c6914e22257a7ea459cf8e0
Tarun Karmakar <tarun.karmakar.dev@gmail.com>
2025-02-02 16:11:25 +0530

    Email Scheduler FE Add brand logo feature

--- apps/email-scheduler-fe/next.config.js
+++ apps/email-scheduler-fe/next.config.js
@@ -1,7 +1,7 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+const { composePlugins, withNx } = require("@nx/next");
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -10,7 +10,7 @@ const nextConfig = {
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
-    svgr: false,
+    svgr: true,
   },
 };
 
---
+++ apps/email-scheduler-fe/src/features/brand-logo/index.tsx
@@ -0,0 +1,9 @@
+import { ReactComponent as Logo } from "./logo.svg";
+
+export default function BrandLogo() {
+  return (
+    <div className="text-white">
+      <Logo />
+    </div>
+  );
+}
