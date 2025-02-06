ae22905237fbe54c3b0e20a55b172eabde875d82
Tarun Karmakar <tarun.karmakar.dev@gmail.com>
2025-02-06 22:07:35 +0530

    Email Scheduler FE Add apiEndpoints config

---
+++ apps/email-scheduler-fe/src/config/index.ts
@@ -0,0 +1,8 @@
+export const apiEndpoints = {
+  googleAuth: "/api/auth/google",
+  templates: "/api/emails/templates",
+  templateDetail: "/api/emails/templates/:id",
+  campaigns: "/api/emails/campaigns",
+  campaignDetail: "/api/emails/campaigns/:id",
+  customers: "/api/emails/customers",
+};
